e0662b9a595697f60a53c185f0403a458c20b579
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-03-06 13:15:53 -0500

    reading data

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,9 @@
+// Create function that gets data and plots
+function getPlot(id) {
+    
+    // Read in the JSON data
+    d3.json("data/samples.json").then((data) => {
+    console.log(data)
+    })
+}
+
