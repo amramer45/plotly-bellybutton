83561e6e7a15d58b23dae2165f0ec3730a2319fb
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-03-09 14:40:25 -0500

    gauge to pie

--- static/js/app.js
+++ static/js/app.js
@@ -99,32 +99,46 @@ function getPlot(id) {
         var data3 = [
             {
                 domain: { x: [0, 1], y: [0, 1] },
-                value: washFrequency,
                 title: { text: "<b>Belly Button Washing Frequency</br> <br> Scrubs Per Week", font: {size: 14} },
-                type: "indicator",
-                mode: "gauge+number",
-                gauge: {
-                    axis: { range: [null, 9] },
-                    bar: { color: "black" },
-                    bordercolor: "transparent",
-                    steps: [
-                        { range: [0, 1], color: "#e5d5e8" },
-                        { range: [1, 2], color: "#ddbce3" },
-                        { range: [2, 3], color: "#d8abe0" },
-                        { range: [3, 4], color: "#d093db" },
-                        { range: [4, 5], color: "#cc82d9" },
-                        { range: [5, 6], color: "#c46ad4" },
-                        { range: [6, 7], color: "#be54d1" },
-                        { range: [7, 8], color: "#b63bcc" },
-                        { range: [8, 9], color: "#9e11b8" },
-                    ],
-                }
+                type: "pie",
+                showlegend: false,
+                hole: 0.5,
+                rotation: 90,
+                text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"],
+                direction: "clockwise",
+                textinfo: "text",
+                textposition: "inside",
+                values: [81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
+                marker: { 
+                    colors: ["#e5d5e8", "#ddbce3", "#d8abe0", "#d093db", "#cc82d9", "#c46ad4", "#be54d1", "#b63bcc", "#9e11b8", "white"],
+                    labels: ["", "0", "1", "2", "3","4","5","6","7","8","9+"],
+                    hoverinfo: "label"
+                },
+                
             }
         ];
 
+        //needle 
+        var degrees = 50, radius = .9
+        var radians = degrees * Math.PI / 180
+        var x = -1 * radius * Math.cos(radians) * washFrequency
+        var y = radius * Math.sin(radians)
 
         //layout
         var layout3 = {
+            shapes: [{
+                type: 'line',
+                x0: 0.5,
+                y0: 0.5,
+                x1: 0.6,
+                y1: 0.6,
+                line: {
+                    color: 'black',
+                    width: 3
+                }
+            }],
+            xaxis: {visible: false, range: [-1, 1]},
+            yaxis: {visible: false, range: [-1, 1]},
             width: 500,
             height: 425,
             margin: {
@@ -135,13 +149,12 @@ function getPlot(id) {
             },
             font: {
                 color: "black",
-                family: "Arial" }
+                family: "Arial" 
+            }
         };
 
         Plotly.newPlot("gauge", data3, layout3);
-
     });
-
 }
 
 function getInfo(id) {
