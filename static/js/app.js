ed4388da2173bc13681186f622ecdc1f597a6537
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-03-09 15:01:17 -0500

    getting rid of hover

--- static/js/app.js
+++ static/js/app.js
@@ -105,6 +105,7 @@ function getPlot(id) {
                 hole: 0.5,
                 rotation: 90,
                 text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"],
+                hoverinfo: "skip",
                 direction: "clockwise",
                 textinfo: "text",
                 textposition: "inside",
@@ -112,7 +113,7 @@ function getPlot(id) {
                 marker: { 
                     colors: ["#e5d5e8", "#ddbce3", "#d8abe0", "#d093db", "#cc82d9", "#c46ad4", "#be54d1", "#b63bcc", "#9e11b8", "white"],
                     labels: ["", "0", "1", "2", "3","4","5","6","7","8","9+"],
-                    hoverinfo: "label"
+                    hoverinfo: "skip"
                 },
                 
             }
