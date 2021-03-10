370a2fda8d7b94f341a167497de9005546ae22ac
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-03-09 19:34:47 -0500

    back to gauge+number

--- static/js/app.js
+++ static/js/app.js
@@ -54,11 +54,6 @@ function getPlot(id) {
 
 
         // ////////// bubble/////////
-        // // Use otu_ids for the x values.
-        // // Use sample_values for the y values.
-        // // Use sample_values for the marker size.
-        // // Use otu_ids for the marker colors.
-        // // Use otu_labels for the text values
 
         var sampleValuesBubble = selectedSample.sample_values;
         var otuIdsBubble = selectedSample.otu_ids;
@@ -91,71 +86,63 @@ function getPlot(id) {
 
         Plotly.newPlot("bubble", data2, layout2);
 
-        //create gauge chart
+        //////////create gauge chart///////////
 
-        var washFrequency = data.metadata.map(d => d.wfreq)
-        console.log(`Washing Frequency: ${washFrequency}`);
-
-        var data3 = [
-            {
-                domain: { x: [0, 1], y: [0, 1] },
-                title: { text: "<b>Belly Button Washing Frequency</br> <br> Scrubs Per Week", font: {size: 14} },
-                type: "pie",
-                showlegend: false,
-                hole: 0.5,
-                rotation: 90,
-                text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"],
-                hoverinfo: "skip",
-                direction: "clockwise",
-                textinfo: "text",
-                textposition: "inside",
-                values: [81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
-                marker: { 
-                    colors: ["#e5d5e8", "#ddbce3", "#d8abe0", "#d093db", "#cc82d9", "#c46ad4", "#be54d1", "#b63bcc", "#9e11b8", "white"],
-                    labels: ["", "0", "1", "2", "3","4","5","6","7","8","9"],
-                    hoverinfo: "skip"
-                },
-                
-            }
-        ];
-
-        //needle 
-        var degrees = 50, radius = .9
-        var radians = degrees * Math.PI / 180
-        var x = -1 * radius * Math.cos(radians) * washFrequency
-        var y = radius * Math.sin(radians)
-
-        //layout
-        var layout3 = {
-            shapes: [{
-                type: 'line',
-                x0: 0.5,
-                y0: 0.5,
-                x1: 0.6,
-                y1: 0.6,
-                line: {
-                    color: 'black',
-                    width: 3
+    var washFrequency = data.metadata.map(d => d.wfreq)
+        //console.log(`Washing Frequency: ${washFrequency}`);
+            var data3 = [
+                {
+                    domain: { x: [0, 1], y: [0, 1] },
+                    values: washFrequency,
+                    title: { text: "<b>Belly Button Washing Frequency</br> <br> Scrubs Per Week", font: {size: 14} },
+                    type: "indicator",
+                    showlegend: false,
+                    hole: 0.4,
+                    rotation: 90,
+                    text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"],
+                    direction: "clockwise",
+                    textinfo: "text",
+                    textposition: "inside",
+                    mode: "gauge+number",
+                    gauge: {
+                        axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue", showticklables: false },
+                        bar: { color: "black" },
+                        bordercolor: "transparent",
+                        steps: [
+                            { range: [0, 1], color: "#e5d5e8" },
+                            { range: [1, 2], color: "#ddbce3" },
+                            { range: [2, 3], color: "#d8abe0" },
+                            { range: [3, 4], color: "#d093db" },
+                            { range: [4, 5], color: "#cc82d9" },
+                            { range: [5, 6], color: "#c46ad4" },
+                            { range: [6, 7], color: "#be54d1" },
+                            { range: [7, 8], color: "#b63bcc" },
+                            { range: [8, 9], color: "#9e11b8" }
+                        ],
+                    }
                 }
-            }],
-            xaxis: {visible: false, range: [-1, 1]},
-            yaxis: {visible: false, range: [-1, 1]},
-            width: 500,
-            height: 425,
-            margin: {
-                t: 20,
-                b: 40,
-                l: 100,
-                r: 100
-            },
-            font: {
-                color: "black",
-                family: "Arial" 
-            }
-        };
-
-        Plotly.newPlot("gauge", data3, layout3);
-    });
+            ];
+                
+            //layout
+            var layout3 = {
+                xaxis: {visible: false, range: [-1, 1]},
+                yaxis: {visible: false, range: [-1, 1]},
+                width: 500,
+                height: 425,
+                margin: {
+                    t: 20,
+                    b: 40,
+                    l: 100,
+                    r: 100
+                },
+                font: {
+                    color: "black",
+                    family: "Arial" 
+                }
+            }    
+        
+    Plotly.newPlot("gauge", data3, layout3);
+});
 }
 
 function getInfo(id) {
@@ -164,7 +151,7 @@ function getInfo(id) {
         
         //get metadata for the info panel
         var metadata = data.metadata
-        console.log(metadata)
+        //console.log(metadata)
     
         //Filter
         var result = data.metadata.filter(item => item.id.toString() === id)[0];
@@ -196,7 +183,7 @@ function init() {
 
     //read the data
     d3.json("./data/samples.json").then((data) => {
-        console.log(data)
+        //console.log(data)
 
         //get the id data to the dropdown menu
         data.names.forEach(function(name) {
@@ -210,4 +197,5 @@ function init() {
     });
 }
 
+//initialize the dashboard 
 init();
